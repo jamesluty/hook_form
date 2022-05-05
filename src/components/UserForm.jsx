874cf0bfae734fd640910e1dc3789b37daca3c24
James Luty <luty0125@gmail.com>
2022-05-05 13:08:04 -0700

    fixed form submit function

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,49 +1,51 @@
 import React, {useState} from 'react'
 import styles from './styles.module.css'
 
-const UserForm = () => {
+const UserForm = (props) => {
 
-    const [firstName, setFirstName] = useState="";
-    const [lastName, setLastName] = useState="";
-    const [email, setEmail] = useState="";
-    const [password, setPassword] = useState="";
-    const [confirmPassword, setConfirmPassword] = useState=""
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-    const updateInfo = (input) => {
-
-    }
+    const createUser = (input) => {
+        input.preventDefault();
+        const newInfo = {firstName, lastName, email, password, confirmPassword};
+    };
 
     return (
         <div className={styles.center}>
-            <form className={styles.column} onSubmit={updateInfo}>
+            <form className={styles.column} onSubmit={createUser}>
                 <div className={styles.inputs}>
                     <label htmlFor="firstName">First Name</label>
-                    <input type="text" onChange={ (input) => setFirstName(input.target.value)} />
+                    <input type="text" onChange={ (input) => setFirstName(input.target.value)}/>
                 </div>
                 <div className={styles.inputs}>
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" onChange={ (input) => setLastName(input.target.value)} />
+                    <input type="text" onChange={ (input) => setLastName(input.target.value)}/>
                 </div>
                 <div className={styles.inputs}>
                     <label htmlFor="email">Email</label>
-                    <input type="text" onChange={ (input) => setEmail(input.target.value)} />
+                    <input type="text" onChange={ (input) => setEmail(input.target.value)}/>
                 </div>
                 <div className={styles.inputs}>
                     <label htmlFor="password">Password</label>
-                    <input type="text" onChange={ (input) => setPassword(input.target.value)} />
+                    <input type="text" onChange={ (input) => setPassword(input.target.value)}/>
                 </div>
                 <div className={styles.inputs}>
                     <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="text" onChange={ (input) => setConfirmPassword(input.target.value)} />
+                    <input type="text" onChange={ (input) => setConfirmPassword(input.target.value)}/>
                 </div>
+                <input className={styles.submit} type="submit" value="Create User"/>
             </form>
             <p className={styles.textAlign}>Your Form Data</p>
             <div className={styles.middle}>
-                <p>First Name: </p>
-                <p>Last Name: </p>
-                <p>Email: </p>
-                <p>Password: </p>
-                <p>Confirm Password: </p>
+                <p>First Name: {firstName}</p>
+                <p>Last Name: {lastName}</p>
+                <p>Email: {email}</p>
+                <p>Password: {password}</p>
+                <p>Confirm Password: {confirmPassword}</p>
             </div>
         </div>
     )
